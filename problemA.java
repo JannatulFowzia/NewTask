8f190b21562a6acdad3ee5ed5f10e0b3ddd88f47
JannatulFowzia <50142089+JannatulFowzia@users.noreply.github.com>
2019-04-30 14:54:38 +0200

    Add files via upload

---
+++ problemA.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class problemA {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        Scanner scan = new Scanner(System.in);
+
+        int testcases = scan.nextInt();
+
+        for (int no = 0; no < testcases; no++)
+            {
+            int couples = scan.nextInt();
+            ArrayList<Integer> nums = new ArrayList<>();
+            ArrayList<Integer> chck = new ArrayList<>();
+            long fin = 0;
+            
+            for (int i = 0; i < couples; i++){
+                nums.add(scan.nextInt());
+                if (!chck.contains(nums.get(i)))
+                    {
+                    chck.add(nums.get(i));
+                    fin += nums.get(i);
+                } else {
+                    fin -= nums.get(i);
+                }
+
+            }
+
+            System.out.println("Case #" + (no + 1) + ": " + fin);
+
+            }
+
+        scan.close();
+        long endTime = System.currentTimeMillis();
+
+        long duration = endTime - startTime;
+        System.out.println(duration);
+            }
+}
