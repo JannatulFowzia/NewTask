668360c874bec6d881bbdb2066efa598351984d6
JannatulFowzia <50142089+JannatulFowzia@users.noreply.github.com>
2019-10-11 08:54:28 +0200

    Update and rename problemA.java to ProblemA.java

--- ProblemA.java
+++ ProblemA.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class problemA {
+public class ProblemA {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
